7280a6b533636ccb48e5252e3c44ef881f70b54c
GreekoASC <greeko2030@gmail.com>
2026-08-15 18:36:15 +0600

    Update SupersonicDropdown.java

--- src/main/java/net/supersonic/gui/widgets/SupersonicDropdown.java
+++ src/main/java/net/supersonic/gui/widgets/SupersonicDropdown.java
@@ -3,6 +3,7 @@ package net.supersonic.gui.widgets;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.PressableWidget;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.text.Text;
 
 public class SupersonicDropdown extends PressableWidget {
@@ -25,14 +26,17 @@ public class SupersonicDropdown extends PressableWidget {
     }
 
     @Override
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {
+        this.appendDefaultNarrations(builder);
+    }
+
+    @Override
     protected void renderButton(DrawContext context, int mouseX, int mouseY, float delta) {
         MinecraftClient client = MinecraftClient.getInstance();
         
-        // Draw dropdown background box
         context.fill(this.getX(), this.getY(), this.getX() + this.width, this.getY() + this.height, 0xFF222222);
         context.drawBorder(this.getX(), this.getY(), this.width, this.height, 0xFF00FFFF);
 
-        // Draw text safely using MinecraftClient textRenderer
         context.drawText(client.textRenderer, this.getMessage(), this.getX() + 10, this.getY() + (this.height - 8) / 2, 0xFFFFFF, false);
     }
 }
